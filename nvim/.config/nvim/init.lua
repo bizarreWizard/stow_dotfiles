f58babe705e13c94e6939102b987e50e9ebbd0cf
Matteo Marrucci <0407matteo.marrucci@gmail.com>
2025-12-05 13:39:25 +0100

    minor changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -33,34 +33,34 @@ vim.cmd("set mouse=a")
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
+    spec = {
     -- add your plugins here
-    {
-    "folke/tokyonight.nvim",    -- tokyonight colorscheme
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    },
+        {
+        "folke/tokyonight.nvim",    -- tokyonight colorscheme
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        },
 
-    {
-    'nvim-telescope/telescope.nvim', -- telescope (find files and live grep)
-    tag = 'v0.2.0',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-    },
+        {
+        'nvim-telescope/telescope.nvim', -- telescope (find files and live grep)
+        tag = 'v0.2.0',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+        },
 
-    {
-    "nvim-treesitter/nvim-treesitter", -- treesitter (language parser and highlighter)
-    branch = 'master', 
-    lazy = false, 
-    build = ":TSUpdate"
-    }
-  },
+        {
+        "nvim-treesitter/nvim-treesitter", -- treesitter (language parser and highlighter)
+        branch = 'master', 
+        lazy = false, 
+        build = ":TSUpdate"
+        }
+    },
   
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "habamax" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
 })
 
 vim.cmd[[colorscheme tokyonight-night]] -- sets tokyonight-night colorscheme
