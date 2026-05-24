7eb321d18f9d289963db2e01ad7cb5868a84dc5d
Matteo Marrucci <0407matteo.marrucci@gmail.com>
2026-05-24 23:56:46 +0200

    thicker font for kitty, more themes for nvim

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -2,6 +2,7 @@ return {
 	{
 		"oskarnurm/koda.nvim",
 		lazy = false,
+		priority = 999,
 		config = function()
 			vim.cmd("colorscheme koda")
 		end,
@@ -11,5 +12,64 @@ return {
 		lazy = false,
 		priority = 1000,
 		opts = {},
+	},
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = false,
+		priority = 1001,
+		config = function()
+			local c = require("vscode.colors").get_colors()
+			require("vscode").setup({
+				-- Alternatively set style in setup
+				-- style = 'light'
+
+				-- Enable transparent background
+				transparent = true,
+
+				-- Enable italic comment
+				italic_comments = true,
+
+				-- Enable italic inlay type hints
+				italic_inlayhints = true,
+
+				-- Underline `@markup.link.*` variants
+				underline_links = true,
+
+				-- Disable nvim-tree background color
+				disable_nvimtree_bg = true,
+
+				-- Apply theme colors to terminal
+				terminal_colors = true,
+
+				-- Override colors (see ./lua/vscode/colors.lua)
+				color_overrides = {
+					vscLineNumber = "#FFFFFF",
+				},
+
+				-- Override highlight groups (see ./lua/vscode/theme.lua)
+				group_overrides = {
+					-- this supports the same val table as vim.api.nvim_set_hl
+					-- use colors from this colorscheme by requiring vscode.colors!
+					Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+				},
+			})
+		end,
+	},
+	{
+		"navarasu/onedark.nvim",
+		priority = 1002,
+		lazy = false,
+		config = function()
+			require("onedark").setup({
+				style = "darker",
+			})
+			require("onedark").load()
+		end,
+	},
+	{
+		"bluz71/vim-moonfly-colors",
+		name = "moonfly",
+		lazy = false,
+		priority = 1003,
 	},
 }
