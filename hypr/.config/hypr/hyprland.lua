4f57ffb62ddaa15f24f194bb16187c36cc2ed168
Matteo Marrucci <0407matteo.marrucci@gmail.com>
2026-08-23 11:29:32 +0200

    hypr: removed window border

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -43,7 +43,7 @@ hl.config({
 	general = {
 		gaps_in = 0,
 		gaps_out = 0,
-		border_size = 1,
+		border_size = 0,
 		col = {
 			active_border = "rgba(40, 85, 119, 1)",
 			inactive_border = "rgba(595959aa)",
