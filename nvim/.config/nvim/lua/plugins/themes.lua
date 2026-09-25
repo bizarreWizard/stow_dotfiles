31bbb6503a459f74f63497967084e8fbbf25ea01
Matteo Marrucci <0407matteo.marrucci@gmail.com>
2026-09-25 15:03:41 +0200

    nvim: added themes

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -57,21 +57,22 @@ return {
 			vim.cmd("colorscheme vscode")
 		end,
 	},
-	{
-		"navarasu/onedark.nvim",
-		priority = 1002,
-		lazy = false,
-		config = function()
-			require("onedark").setup({
-				style = "darker",
-			})
-			require("onedark").load()
-		end,
-	},
-	{
-		"bluz71/vim-moonfly-colors",
-		name = "moonfly",
-		lazy = false,
-		priority = 1003,
-	},
+    {
+        "zenbones-theme/zenbones.nvim",
+        -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+        -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+        -- In Vim, compat mode is turned on as Lush only works in Neovim.
+        dependencies = "rktjmp/lush.nvim",
+        lazy = false,
+        priority = 1002,
+        -- you can set set configuration options here
+        -- config = function()
+            --     vim.g.zenbones_darken_comments = 45
+            --     vim.cmd.colorscheme('zenbones')
+            -- end
+        },
+        {
+            "rebelot/kanagawa.nvim",
+            priority = 1003,
+        }
 }
